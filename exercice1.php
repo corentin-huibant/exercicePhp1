1f52093d3c252925a8f9ac5721c2b547a5f1bbec
Corentin Huibant <Corentin.huibant@laposte.net>
2017-01-28 19:15:15 +0100

    Le premier exercice PHP : les variables, addition et multiplication

---
+++ exercice1.php
@@ -0,0 +1,13 @@
+<?php
+$variable1 = 7;
+$variable2 = 8;
+echo $variable1;
+echo '&nbsp;';
+echo $variable2;
+echo '&nbsp;';
+$addition = 7 + 8;
+$multiplication =  7 * 8;
+echo $addition;
+echo '&nbsp;';
+echo $multiplication;
+?>
